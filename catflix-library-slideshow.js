59af66e46f1858fabbc245f41a115c00aa87162c
Burtan <brajanwow@gmail.com>
2026-06-12 22:29:57 +0200

    Update catflix-library-slideshow.js

--- catflix-library-slideshow.js
+++ catflix-library-slideshow.js
@@ -7,7 +7,7 @@
     const LIBRARY_IMAGES = {
         'movies': [
             `${CDN}/Movies-1.png`,
-            `${CDN}/Movies-2.png`,
+            `${CDN}/Movies-2.jpg`,
         ],
         'tvshows': [
             `${CDN}/Shows-1.png`,
